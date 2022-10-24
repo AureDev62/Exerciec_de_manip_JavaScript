7fc93db8edb735340544e685c8251c37e9da51d4
AureDev62 <audioslave29@gmail.com>
2022-10-24 11:02:17 +0200

    fin_D_Exo

---
+++ script.js
@@ -0,0 +1,24 @@
+let ville='Cambrai';
+
+const hypothenuse=8;
+
+let admission='vrai';
+
+let nom='Moi';
+
+let prénom='Aurélien';
+
+let âge=42;
+
+let pommes= 4;
+
+let pêches= 6;
+
+let totalFruits=(pommes *pêches);
+
+console.log (totalFruits);
+
+function addition(a, b) {
+    return a+b
+}
+ console.log(addition(2,4))
